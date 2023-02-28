501fefcecbc5350cae064019da62d2b38b71b665
VisheshAtWork <visheshatwork@gmail.com>
2023-02-28 19:30:58 +0530

    run & verify

--- lib/simpleInterestPage.dart
+++ lib/simpleInterestPage.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'dart:async';
 
-
 const List<Widget> purity = <Widget>[
   Text('2'),
   Text('2.5'),
   Text('3'),
   Text('4'),
 ];
-
-
 
 class SimpleInterestPage extends StatefulWidget {
   const SimpleInterestPage({super.key});
@@ -21,22 +18,21 @@ class SimpleInterestPage extends StatefulWidget {
 }
 
 class _SimpleInterestPageState extends State<SimpleInterestPage> {
-  int
-      numberOfMonths = 0,
+  int numberOfMonths = 0,
       monthsInterestPaid = 0,
       pa = 0,
-      numberOfDays =0,
+      numberOfDays = 0,
       amountPaid = 0,
       remainingMonths = 0,
       interestAmount = 0,
       hundred = 100;
-
 
   double rateOfInterest = 0;
 
   final List<bool> _selectedPurity = <bool>[true, false, false, false];
   DateTime date = DateTime.now();
   TextEditingController dateInput = TextEditingController();
+  bool? release;
 
   @override
   void initState() {
@@ -46,7 +42,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: SingleChildScrollView(
         child: Center(
@@ -63,7 +58,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 25,
-
                       fontFamily: 'Lato',
                       fontWeight: FontWeight.bold),
                 ),
@@ -95,80 +89,85 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                 ),
                 child: Column(
                   children: [
-
-                       Padding(
-                         padding: const EdgeInsets.only(bottom: 30),
-                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right:40),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(right: 40),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Text('Date:',
+                                      style: TextStyle(fontSize: 16)),
+                                ),
+                                Text(
+                                    DateFormat('dd/MM/yyyy')
+                                        .format(DateTime.now()),
+                                    style: TextStyle(fontSize: 18)),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 30.0),
                               child: Column(
                                 children: [
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: Text('Date:',
-                                        style: TextStyle(fontSize: 16)),
+                                  Row(
+                                    children: [
+                                      Text('Bill Date:',
+                                          style: TextStyle(fontSize: 16)),
+                                      IconButton(
+                                        onPressed: () async {
+                                          DateTime? pickedDate =
+                                              await showDatePicker(
+                                                  context: context,
+                                                  initialDate: DateTime.now(),
+                                                  firstDate: DateTime(2019),
+                                                  //DateTime.now() - not to allow to choose before today.
+                                                  lastDate: DateTime(2024));
+
+                                          if (pickedDate != null) {
+                                            print(pickedDate);
+                                            String formattedDate =
+                                                DateFormat('dd/MM/yyyy')
+                                                    .format(pickedDate);
+                                            print(
+                                                formattedDate); //formatted date output using intl package =>  2021-03-16
+                                            setState(() {
+                                              dateInput.text = formattedDate;
+                                              Duration diff = DateTime.now()
+                                                  .difference(pickedDate);
+                                              // ignore: division_optimization
+                                              int diffMonths =
+                                                  (diff.inDays / 30).toInt();
+                                              int tempDays = (diff.inDays);
+                                              print(diffMonths);
+                                              //this is working
+                                              numberOfMonths = diffMonths;
+                                              numberOfDays =
+                                                  tempDays - (diffMonths * 30);
+                                            });
+                                          } else {}
+                                        },
+                                        icon: Icon(
+                                          Icons.edit,
+                                          size: 20,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  Text(DateFormat('dd/MM/yyyy').format(DateTime.now()), style: TextStyle(fontSize: 18)),
+                                  Text('${dateInput.text}',
+                                      style: TextStyle(fontSize: 18)),
                                 ],
                               ),
                             ),
-
-
-                            Container(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left:30.0),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                            children: [
-                                              Text('Bill Date:',
-                                                  style: TextStyle(fontSize: 16)),
-                                              IconButton(onPressed: () async {
-                                                DateTime? pickedDate = await showDatePicker(
-                                                    context: context,
-                                                    initialDate: DateTime.now(),
-                                                    firstDate: DateTime(2019),
-                                                    //DateTime.now() - not to allow to choose before today.
-                                                    lastDate: DateTime(2024));
-
-                                                if (pickedDate != null) {
-                                                  print(pickedDate);
-                                                  String formattedDate =
-                                                  DateFormat('dd/MM/yyyy').format(pickedDate);
-                                                  print(
-                                                      formattedDate); //formatted date output using intl package =>  2021-03-16
-                                                  setState(() {
-                                                    dateInput.text =
-                                                        formattedDate;
-                                                    Duration diff = DateTime.now().difference(pickedDate);
-                                                    // ignore: division_optimization
-                                                    int diffMonths = (diff.inDays / 30).toInt();
-                                                    int tempDays = (diff.inDays);
-                                                    print(diffMonths);
-                                                    //this is working
-                                                    numberOfMonths = diffMonths;
-                                                    numberOfDays =  tempDays - (diffMonths * 30);
-                                                  });
-                                                } else {}
-                                              },
-                                                icon: Icon(Icons.edit, size: 20,),),
-
-
-
-                                      ],
-                                    ),
-                                    Text('${dateInput.text}' , style: TextStyle(fontSize: 18)),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
+                          ),
+                        ],
                       ),
-                       ),
-
-
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 6,
@@ -185,11 +184,10 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                           ),
                           Expanded(
                             child: TextField(
-
                               textInputAction: TextInputAction.next,
                               keyboardType:
-                              const TextInputType.numberWithOptions(
-                                  decimal: true),
+                                  const TextInputType.numberWithOptions(
+                                      decimal: true),
                               decoration: const InputDecoration(
                                 border: UnderlineInputBorder(),
                                 isDense: true,
@@ -206,7 +204,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                         ],
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 6,
@@ -226,15 +223,17 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                               onPressed: (int index) async {
                                 setState(() {
                                   // The button that is tapped is set to true, and the others to false.
-                                  for (int i = 0; i < _selectedPurity.length; i++) {
+                                  for (int i = 0;
+                                      i < _selectedPurity.length;
+                                      i++) {
                                     _selectedPurity[i] = i == index;
                                     if (index == 0) {
                                       rateOfInterest = 2;
-                                    } else if (index == 1){
+                                    } else if (index == 1) {
                                       rateOfInterest = 2.5;
-                                    } else if (index == 2){
-                                      rateOfInterest = 3;}
-                                    else{
+                                    } else if (index == 2) {
+                                      rateOfInterest = 3;
+                                    } else {
                                       rateOfInterest = 4;
                                     }
                                   }
@@ -244,7 +243,8 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                                 minHeight: 40.0,
                                 minWidth: 49,
                               ),
-                              borderRadius: const BorderRadius.all(Radius.circular(8)),
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
                               selectedColor: Colors.white,
                               fillColor: Colors.green,
                               isSelected: _selectedPurity,
@@ -254,7 +254,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                         ],
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 6,
@@ -269,9 +268,22 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                               style: TextStyle(fontSize: 16),
                             ),
                           ),
-                          Text('${numberOfMonths.toString()} ${numberOfDays.toString()}'),
+                          Text(
+                              '${numberOfMonths.toString()} ${numberOfDays.toString()}'),
                         ],
                       ),
+                    ),
+                    Row(
+                      children: [
+                        Switch(
+                            value: release!,
+                            onChanged: (value) {
+                              setState(() {
+                                release = value;
+                              });
+                            }),
+                        Text('Release?')
+                      ],
                     ),
                     Padding(
                       padding: const EdgeInsets.only(
@@ -289,11 +301,10 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                           ),
                           Expanded(
                             child: TextField(
-
                               textInputAction: TextInputAction.next,
                               keyboardType:
-                              const TextInputType.numberWithOptions(
-                                  decimal: true),
+                                  const TextInputType.numberWithOptions(
+                                      decimal: true),
                               decoration: const InputDecoration(
                                 border: UnderlineInputBorder(),
                                 isDense: true,
@@ -303,13 +314,13 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                               onChanged: (value) {
                                 setState(() {
                                   monthsInterestPaid = int.parse(value);
-                                  remainingMonths = numberOfMonths - monthsInterestPaid;
-                                 //not working  interestAmount =  (pa * rateofinterest )/100 * remainingMonths;
-                                 // total amount = pa + interestAmount - amountPaid
-
+                                  remainingMonths =
+                                      numberOfMonths - monthsInterestPaid;
+                                  interestAmount =
+                                      ((pa * rateOfInterest.toInt()) / 100) *
+                                          remainingMonths as int;
+                                  totalAmount = (interestAmount + pa) as double;
                                 });
-
-
                               },
                             ),
                           ),
@@ -334,7 +345,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                         ],
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 6,
@@ -351,11 +361,10 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                           ),
                           Expanded(
                             child: TextField(
-
                               textInputAction: TextInputAction.next,
                               keyboardType:
-                              const TextInputType.numberWithOptions(
-                                  decimal: true),
+                                  const TextInputType.numberWithOptions(
+                                      decimal: true),
                               decoration: const InputDecoration(
                                 border: UnderlineInputBorder(),
                                 isDense: true,
@@ -372,7 +381,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                         ],
                       ),
                     ),
-
                     Padding(
                       padding: const EdgeInsets.only(
                         top: 6,
@@ -380,26 +388,23 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                       ),
                       child: Row(
                         children: [
-                          const Padding(
+                          Padding(
                             padding: EdgeInsets.all(8.0),
                             child: Text(
-                              'Interest Amount: ',
+                              'Interest Amount: $interestAmount',
                               style: TextStyle(fontSize: 16),
                             ),
                           ),
-                          Text(''),
+                          if (release!) Text('Total Amount $totalAmount'),
                         ],
                       ),
                     ),
-
-
                     SizedBox(
                       height: 25,
                     ),
-
                     SizedBox(
-                      height:50, //height of button
-                      width:350,
+                      height: 50, //height of button
+                      width: 350,
 
                       /*child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
@@ -417,7 +422,6 @@ class _SimpleInterestPageState extends State<SimpleInterestPage> {
                         child: const Text('Calculate'),
                       ),*/
                     ),
-
                     SizedBox(
                       height: 10,
                     ),
